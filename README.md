9341c1912af704c3805011768b5a76033e599323
nikkoScript <nicolasspagnulo@gmail.com>
2022-07-09 23:11:52 +0200

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+# ProjectSNK
+
+Developed with Unreal Engine 4
---
+++ Source/ProjectSNK/ProjectSNK.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectSNK.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectSNK, "ProjectSNK" );
